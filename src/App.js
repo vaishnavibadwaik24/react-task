0f6c9370d67632235539077cc7c4b75e21899540
Vaishnavi A Badwaik <88033256+vaishnavibadwaik24@users.noreply.github.com>
2024-07-09 17:08:21 +0530

    contact section added

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ const Home = () => (
 
     {/* Start Project */}
     <section id='Project' className="text-black body-font">
-      <h2 className="text-3xl font-semibold tracking-wide pt-16 ml-8">Check my Projects</h2>
+      <h2 className="text-4xl font-bold underline tracking-wide pt-16 ml-8">Check my Projects</h2>
       <div className="container px-5 py-16 mx-auto">
         <div className="flex flex-wrap -m-4">
           {/* Card 1 */}
@@ -145,6 +145,75 @@ const Home = () => (
       </div>
     </section>
     {/* End Project */}
+
+    {/* Start Contact */}
+    <section id='Contact' className="container mx-auto py-16 px-4 md:px-8 lg:px-16">
+      <h1 className="text-5xl font-bold pb-20 pt-10 underline text-center">Say Hi!</h1>
+  <div className="w-full max-w-5xl mx-auto p-8 space-y-6 rounded-xl bg-white shadow-lg flex flex-col md:flex-row">
+    {/* Left Side Form */}
+    <form noValidate className="w-full md:w-2/3 p-6 space-y-6">
+      <div className="flex flex-col md:flex-row gap-6">
+        <div className="w-full">
+          <label className="block mb-1 text-gray-700">Name</label>
+          <input
+            type="text"
+            placeholder="Your Name"
+            className="w-80 border-b-2 border-gray-300 focus:border-black focus:outline-none py-2"
+          />
+        </div>
+      </div>
+      <div className="w-full">
+        <label className="block mb-1 text-gray-700">Email</label>
+        <input
+          type="email"
+          placeholder="Your Email"
+          className="w-80 border-b-2 border-gray-300 focus:border-black focus:outline-none py-2"
+        />
+      </div>
+      <div className="w-full">
+        <label className="block mb-1 text-gray-700">Your Message</label>
+        <textarea
+          rows="4"
+          placeholder="Your Message"
+          className="w-80 border-b-2 border-gray-300 focus:border-black focus:outline-none py-2"
+        ></textarea>
+      </div>
+      <button
+        type="button"
+        className="bg-black text-white px-6 py-2 rounded-full self-start ml-20"
+      >
+        Send Message
+      </button>
+    </form>
+    {/* Right Side Content */}
+    <div className="w-full md:w-1/2 bg-black text-white p-6 rounded-xl space-y-6">
+      <h2 className="text-2xl font-bold px-8 mt-10">Contact Information</h2>
+      <p className='px-8'>Fill up the form and will get back to you within 24 hours.</p>
+      <div className="space-y-4">
+        <p className="flex items-center px-8">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6 mr-2 sm:mr-5">
+						<path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
+					</svg>
+          <span>8805881762</span>
+        </p>
+        <p className="flex items-center px-8">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-7 h-7 mr-2 sm:mr-5">
+						<path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+						<path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+					</svg>
+          <span>vaishnavib2428@gmail.com</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+
+    {/* End Contact */}
+
   </>
 );
 
