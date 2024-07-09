ad1d79015b9e05518928313194c803817a616f2b
Vaishnavi A Badwaik <88033256+vaishnavibadwaik24@users.noreply.github.com>
2024-07-09 17:50:43 +0530

    Header updated

--- src/App.js
+++ src/App.js
@@ -6,38 +6,41 @@ import g5 from './images/g5.png';
 import g1 from './images/g1.png';
 import g3 from './images/g3.png';
 import Projects from './Projects.jsx';
+import resume from './images/resume.png'
 
 function App() {
   return (
     <Router>
       <>
         {/* Start Header */}
-        <header className="p-4 dark:bg-black dark:text-white">
-          <div className="container flex justify-between h-16 mx-auto">
-            <li className="flex">
-              <Link to="/" className="flex items-center px-4 -mb-1 text-lg font-bold">My Portfolio</Link>
-            </li>
-            <ul className="items-stretch hidden space-x-3 lg:flex">
-              <li className="flex">
-                <a href="/" className="flex items-center px-4 -mb-1">Home</a>
-              </li>
-              <li className="flex">
-                <a href="#About" className="flex items-center px-4 -mb-1">About Us</a>
-              </li>
-              <li className="flex">
-                <a href="#Contact" className="flex items-center px-4 -mb-1">Contact Us</a>
-              </li>
-            </ul>
-            <div className="items-center flex-shrink-0 hidden lg:flex">
-              <a href="#Project" className="self-center px-8 py-3 font-semibold rounded dark:bg-white dark:text-black">Projects</a>
-            </div>
-            <button className="p-4 lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-800">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
-            </button>
-          </div>
-        </header>
+        <header className="p-4 dark:bg-black dark:text-white px-20">
+  <div className="container flex justify-between h-16 mx-auto items-center">
+    <div className="flex items-center">
+      <Link to="/" className="flex items-center px-4 -mb-1 text-2xl font-bold">My Portfolio</Link>
+    </div>
+    <ul className="items-stretch hidden space-x-3 lg:flex">
+      <li className="flex">
+        <a href="/" className="flex items-center px-4 -mb-1">Home</a>
+      </li>
+      <li className="flex">
+        <a href="#About" className="flex items-center px-4 -mb-1">About Us</a>
+      </li>
+      <li className="flex">
+        <a href="#Contact" className="flex items-center px-4 -mb-1">Contact Us</a>
+      </li>
+    </ul>
+    <div className="items-center hidden lg:flex space-x-4">
+      <a href={resume} target='_blank' className="self-center px-6 py-2 font-semibold rounded dark:bg-white dark:text-black">View CV</a>
+      <a href="#Project" className="self-center px-6 py-2 font-semibold rounded dark:bg-white dark:text-black">Projects</a>
+    </div>
+    <button className="p-4 lg:hidden">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-800">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+      </svg>
+    </button>
+  </div>
+</header>
+
         {/* End Header */}
         <Routes>
           <Route path="/" element={<Home />} />
